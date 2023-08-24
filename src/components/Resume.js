9586ea70d71019e577d9eb622a20890de6c749b3
Stephen Werbeck <SWerbeck@yahoo.com>
2023-08-24 04:50:30 -0400

    resume viewable

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,16 @@
-import React from "react";
+import {React, } from "react";
+
 import swerbeckresume from "../elements/swerbeckresume.pdf"
 
 
 
 const Resume = () => {
+    
 return(
 <div>
-    <img src={swerbeckresume} alt="logo"/>
+<object data={swerbeckresume} type="application/pdf" width="100%" height="auto" background="green">
+      <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+  </object>
 </div>
 
 )}
