44688b2f7a192e619b0b729f0d253c281ed5b94f
Stephen Werbeck <SWerbeck@yahoo.com>
2023-10-18 14:07:56 -0400

    started projects page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Resume from './components/Resume.js';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
-     <Resume />
+     <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,59 @@
+import {React, } from "react";
+
+
+
+
+
+const Projects = () => {
+    
+return(
+    /**/
+<div>  
+   {/*limit planning*/}
+    <div>
+    <div>
+        LIMIT PLANNING
+    </div>
+    <div>
+        Limit is a financial planning app that allows users to get a projected 
+        balance, on their banking account, on any given date, by inputing normal monthly expenses and earnings.
+    </div>
+    <div>
+        Lesson learned: 
+    </div>
+    </div>
+       {/*LAST coffee*/}
+       <div>
+    <div>
+        LIMIT PLANNING
+    </div>
+    <div>
+        Limit is a financial planning app that allows users to get a projected 
+        balance, on their banking account, on any given date, by inputing normal monthly expenses and earnings.
+    </div>
+    <div>
+        Lesson learned: 
+    </div>
+    </div>
+
+       {/*timely manner*/}
+       <div>
+    <div>
+        LIMIT PLANNING
+    </div>
+    <div>
+        Limit is a financial planning app that allows users to get a projected 
+        balance, on their banking account, on any given date, by inputing normal monthly expenses and earnings.
+    </div>
+    <div>
+        Lesson learned: 
+    </div>
+    </div>
+
+
+
+</div>
+
+)}
+
+export default Projects
